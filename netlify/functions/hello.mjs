b33b194a467610ea51cfc551fb2dba4c396226cc
ylib <ylib@outlook.com>
2025-07-04 13:15:44 -0500

    Add files via upload

---
+++ netlify/functions/hello.mjs
@@ -0,0 +1,8 @@
+        // functions/hello-world.js
+        exports.handler = async (event) => {
+          const subject = event.queryStringParameters.name || 'World';
+          return {
+            statusCode: 200,
+            body: `Hello ${subject}!`,
+          };
+        };
