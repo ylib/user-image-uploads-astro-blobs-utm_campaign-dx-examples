76cf8413b72b4313ae15e3a6302606ba9a6ba157
ylib <ylib@outlook.com>
2025-07-04 13:49:51 -0500

    Try js extension

---
+++ netlify/functions/jokes.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+    const url = "https://icanhazdadjoke.com/";
+    try {
+        const jokeStream = await fetch(url, {headers: {Accept: "application/json"}});
+        const jsonJoke = await jokeStream.json();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(jsonJoke)
+        }
+    } catch (err) {
+        return {statusCode: 422, body: err.stack};
+    }
+};
