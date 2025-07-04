ff299cbd09e1c8b8f85455cbbe379816cea029df
ylib <ylib@outlook.com>
2025-07-04 18:36:04 -0500

    Add jokes blob

--- netlify/functions/jokes.js
+++ netlify/functions/jokes.js
@@ -1,13 +1,20 @@
 exports.handler = async (event, context) => {
     const url = "https://icanhazdadjoke.com/";
-    try {
-        const jokeStream = await fetch(url, {headers: {Accept: "application/json"}});
-        const jsonJoke = await jokeStream.json();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(jsonJoke)
+    const store = getStore('jokes');
+
+    if (Math.random()) {
+        const {blobs: jokes} = await store.list();
+        return {statusCode: 200, body: JSON.stringify(jokes)};
+    } else {
+        try {
+            const jokeStream = await fetch(url, {headers: {Accept: "application/json"}});
+            const jsonJoke = await jokeStream.json();
+            return {
+                statusCode: 200,
+                body: JSON.stringify(jsonJoke)
+            }
+        } catch (err) {
+            return {statusCode: 422, body: err.stack};
         }
-    } catch (err) {
-        return {statusCode: 422, body: err.stack};
     }
 };
