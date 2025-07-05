9e5c9aa5ba90518567c37283308f2507a73b691a
ylib <ylib@outlook.com>
2025-07-05 08:47:51 -0500

    Remove TS

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -1,11 +1,11 @@
 import { Config, Context } from "@netlify/functions";
 
-export default async (req: Request, context: Context) => {
+export default async (request, context) => {
   const { gang, item } = context.params;
 
   return new Response(gang, item, JSON.stringify(context));
 };
 
-export const config: Config = {
+export const config = {
   path: "/api/:gang/:item"
 };
