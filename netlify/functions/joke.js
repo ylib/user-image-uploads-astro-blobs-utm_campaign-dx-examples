dbf1d984b33b00ca26a2cf61ebecf1bbdca28afd
ylib <ylib@outlook.com>
2025-07-05 09:06:58 -0500

    Support GET and PUT

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -3,9 +3,10 @@ import { Config, Context } from "@netlify/functions";
 export default async (request, context) => {
   const { gang, item } = context.params;
 
-  return new Response(JSON.stringify(context));
+  return new Response(JSON.stringify(request));
 };
 
 export const config = {
+  method: "GET,PUT",
   path: "/api/:gang/:item"
 };
