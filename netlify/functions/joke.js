8d17f3ce463eabe634deb99a6726045703390951
ylib <ylib@outlook.com>
2025-07-05 09:37:54 -0500

    Await request.json

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -2,9 +2,14 @@ import { Config, Context } from "@netlify/functions";
 
 export default async (request, context) => {
   const { gang, item } = context.params;
-  const {method, body, data} = request;
+  const {method} = request;
 
-  return new Response(JSON.stringify({method, body, data}));
+  if (method === 'PUT') {
+    const data = await request.json();
+    return new Response(JSON.stringify({method, data}));
+  } else {
+    return new Response(JSON.stringify({gang, item}));
+  }
 };
 
 export const config = {
