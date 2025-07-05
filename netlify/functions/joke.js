4b4e30393b397fbeeccc695532fc2fafd584a3ee
ylib <ylib@outlook.com>
2025-07-05 09:26:20 -0500

    Add OPTIONS

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -2,11 +2,12 @@ import { Config, Context } from "@netlify/functions";
 
 export default async (request, context) => {
   const { gang, item } = context.params;
+  const {method, httpMethod} = request;
 
-  return new Response(JSON.stringify(request));
+  return new Response(JSON.stringify({method, httpMethod}));
 };
 
 export const config = {
-  method: ['GET','PUT'],
+  method: ['GET','OPTIONS','PUT'],
   path: "/api/:gang/:item"
 };
